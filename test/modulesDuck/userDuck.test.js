dd9bacb9a3f0b33da6c54dbbfbb657f5c6c9e958
Nguyen Tung Lam <tunglam20132208@gmail.com>
2019-12-12 20:48:56 +0700

    add tag test

--- test/modulesDuck/userDuck.test.js
+++ test/modulesDuck/userDuck.test.js
@@ -20,7 +20,7 @@ describe("userduck", () => {
         }
       };
 
-      const store = createStore({ mode: MODE_TEST_STORE.LOG });
+      const store = createStore({ mode: MODE_TEST_STORE.TEST });
       store.subscribe(() => {
         const { previusState, newState, type } = getStore(store);
         if (
