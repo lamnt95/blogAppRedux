b58b3459debe562ce700aed223d3ba866bda0472
Nguyen Tung Lam <tunglam20132208@gmail.com>
2019-12-16 10:18:43 +0700

    fix congif

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ import createAppStore from "./src";
 import reducers from "./src/reducers";
 import { MODE_TEST_STORE } from "./src/constant";
 
-export const MODE_STORE = {...MODE_TEST_STORE}
-export const reducers;
-export const createAppStore;
-export default {};
+export default { reducers, createAppStore, MODE_TEST_STORE };
